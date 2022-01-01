b0160c566762c760d5a3cc219b6fe766a0417ed7
Padhiver <80126915+Padhiver@users.noreply.github.com>
2022-01-01 13:35:00 +0100

    Update data_movement.js

--- js/data_movement.js
+++ js/data_movement.js
@@ -1,39 +1,5 @@
 data_movement = [
     {
-        title: "Se déplacer",
-        icon: "run",
-        subtitle: "Coût: Vitesse utilisée",
-        description: "Coût: Vitesse de chaque mode de déplacement",
-        reference: "PHB, pg. 190.",
-        bullets: [
-            "Lors de votre tour, vous pouvez vous déplacer sur une distance inférieure ou égale à votre vitesse. Pendant votre déplacement, vous pouvez combiner ou alterner le saut, l'escalade ou la nage. Vous devez soustraire à chaque mode de déplacement le coût correspondant à votre vitesse, et vous arrêter si votre vitesse est réduite à 0.",
-            "Vous pouvez traverser un emplacement occupé par une créature amie.",
-            "Vous ne pouvez traverser un emplacement occupé par une créature hostile, sauf si celle-ci fait à minima deux catégories de taille de moins que vous.",
-            "L'emplacement occupé par une créature est toujours considéré comme un terrain difficile.",
-            "Si vous sortez de la zone d'allonge d'une créature hostile pendant votre déplacement, vous provoquez une attaque d'opportunité contre vous."
-        ]
-    },
-    {
-        title: "Escalader",
-        icon: "crags",
-        subtitle: "Coût: 1 mètre coûte 2",
-        description: "Coût du déplacement : 1 mètre escaladé coûte 2 mètres de déplacement normal ou plus si terrain difficile",
-        reference: "PHB, pg. 182.",
-        bullets: [
-            "Un test de Force (Athlétisme) peut vous être demandé par le MD en fonction du terrain escaladé"
-        ]
-    },
-    {
-        title: "Nager",
-        icon: "at-sea",
-        subtitle: "Coût: 1 mètre coûte 2",
-        description: "Coût du déplacement : 1 mètre nagé coûte 2 mètres de déplacement normal ou plus si eaux agitées",
-        reference: "PHB, pg. 182.",
-        bullets: [
-            "Un test de Force (Athlétisme) peut vous être demandé par le MD si les conditions de natation sont difficiles"
-        ]
-    },
-    {
         title: "Chute",
         icon: "falling",
         subtitle: "Coût: 0",
@@ -45,7 +11,47 @@ data_movement = [
             "Le maximum de dégâts est de 20d6, sauf si vous pouvez les éviter, sinon vous serez à terre à la fin de votre chute."
         ]
     },
-    {
+	{
+        title: "Créature Empoignée",
+        icon: "grab",
+        subtitle: "Coût: Vitesse divisée par 2",
+        description: "Déplacer une créature que vous avez empoignée",
+        reference: "PHB, pg. 195.",
+        bullets: [
+            "Vous vous déplacez en traînant ou portant la créature, mais votre vitesse est divisée par deux. Sauf si la créature fait au minimum <b> 2 catégories de taille de moins </b> que vous.",
+            "L'empoignade est une action d'attaque au corps à corps spéciale."
+        ]
+    },
+	 {
+        title: "Escalader",
+        icon: "crags",
+        subtitle: "Coût: 1 mètre coûte 2",
+        description: "Coût du déplacement : 1 mètre escaladé coûte 2 mètres de déplacement normal ou plus si terrain difficile",
+        reference: "PHB, pg. 182.",
+        bullets: [
+            "Un test de Force (Athlétisme) peut vous être demandé par le MD en fonction du terrain escaladé"
+        ]
+    },
+	    {
+        title: "Improviser un déplacement",
+        icon: "juggler",
+        subtitle: "Autre déplacement non listé ici",
+        description: "Réaliser un déplacement particulier",
+        bullets: [
+            "Si un déplacement, mouvement ou cascade n'est pas décrit dans les règles, parlez en à votre MD pour savoir si ce déplacement est autorisé, et si un jet de caractéristique est nécessaire pour en déterminer le succès ou l'échec."
+        ]
+    },
+	 {
+        title: "Nager",
+        icon: "at-sea",
+        subtitle: "Coût: 1 mètre coûte 2",
+        description: "Coût du déplacement : 1 mètre nagé coûte 2 mètres de déplacement normal ou plus si eaux agitées",
+        reference: "PHB, pg. 182.",
+        bullets: [
+            "Un test de Force (Athlétisme) peut vous être demandé par le MD si les conditions de natation sont difficiles"
+        ]
+    },
+	    {
         title: "Ramper",
         icon: "crawl",
         subtitle: "Coût: 1 mètre coûte 2",
@@ -55,17 +61,7 @@ data_movement = [
 			"Un test de Force (Athlétisme) peut vous être demandé par le MD si vous devez ramper sur un terrain difficile"
         ]
     },
-    {
-        title: "Se Relever",
-        icon: "strong",
-        subtitle: "Coût: Moitié de la vitesse",
-        description: "Coût du déplacement : La Moitié de votre vitesse totale",
-        reference: "PHB, pg. 190-191.",
-        bullets: [
-            "Pour vous relever, cela vous coûte la moitié de votre vitesse totale (4,50 mètres si votre vitesse est de 9). De plus impossible de vous relevez si votre vitesse est insuffisante ou réduite à 0."
-        ]
-    },
-    {
+	 {
         title: "Saut en hauteur",
         icon: "wingfoot",
         subtitle: "Coût: 1 mètre coûte 1",
@@ -92,16 +88,31 @@ data_movement = [
             "Vous devez réussir un test de Dextérité (Acrobaties) DD10 pour retomber sur vos pieds sur un terrain difficile. Sinon vous tombez à terre."
         ]
     },
-    {
-        title: "Improviser un déplacement",
-        icon: "juggler",
-        subtitle: "Autre déplacement non listé ici",
-        description: "Réaliser un déplacement particulier",
+	{
+        title: "Se déplacer",
+        icon: "run",
+        subtitle: "Coût: Vitesse utilisée",
+        description: "Coût: Vitesse de chaque mode de déplacement",
+        reference: "PHB, pg. 190.",
         bullets: [
-            "Si un déplacement, mouvement ou cascade n'est pas décrit dans les règles, parlez en à votre MD pour savoir si ce déplacement est autorisé, et si un jet de caractéristique est nécessaire pour en déterminer le succès ou l'échec."
+            "Lors de votre tour, vous pouvez vous déplacer sur une distance inférieure ou égale à votre vitesse. Pendant votre déplacement, vous pouvez combiner ou alterner le saut, l'escalade ou la nage. Vous devez soustraire à chaque mode de déplacement le coût correspondant à votre vitesse, et vous arrêter si votre vitesse est réduite à 0.",
+            "Vous pouvez traverser un emplacement occupé par une créature amie.",
+            "Vous ne pouvez traverser un emplacement occupé par une créature hostile, sauf si celle-ci fait à minima deux catégories de taille de moins que vous.",
+            "L'emplacement occupé par une créature est toujours considéré comme un terrain difficile.",
+            "Si vous sortez de la zone d'allonge d'une créature hostile pendant votre déplacement, vous provoquez une attaque d'opportunité contre vous."
         ]
     },
     {
+        title: "Se Relever",
+        icon: "strong",
+        subtitle: "Coût: Moitié de la vitesse",
+        description: "Coût du déplacement : La Moitié de votre vitesse totale",
+        reference: "PHB, pg. 190-191.",
+        bullets: [
+            "Pour vous relever, cela vous coûte la moitié de votre vitesse totale (4,50 mètres si votre vitesse est de 9). De plus impossible de vous relevez si votre vitesse est insuffisante ou réduite à 0."
+        ]
+    },
+	{
         title: "Terrain difficile",
         icon: "stone-pile",
         subtitle: "Coût: 1 mètre coûte 2",
@@ -109,16 +120,6 @@ data_movement = [
         description: "En terrain difficile votre distance de déplacement ne pourra atteindre que la moitié de votre déplacement normal. Votre vitesse est divisée par deux.",
         bullets: [
         ]
-    },
-    {
-        title: "Créature Empoignée",
-        icon: "grab",
-        subtitle: "Coût: Vitesse divisée par 2",
-        description: "Déplacer une créature que vous avez empoignée",
-        reference: "PHB, pg. 195.",
-        bullets: [
-            "Vous vous déplacez en traînant ou portant la créature, mais votre vitesse est divisée par deux. Sauf si la créature fait au minimum <b> 2 catégories de taille de moins </b> que vous.",
-            "L'empoignade est une action d'attaque au corps à corps spéciale."
-        ]
     }
+
 ]
