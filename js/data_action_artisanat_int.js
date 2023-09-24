8663f22c692e60835841e6bf59d55deea625528b
Padhiver <80126915+Padhiver@users.noreply.github.com>
2023-09-24 18:07:13 +0200

    Add files via upload

---
+++ js/data_action_artisanat_int.js
@@ -0,0 +1,51 @@
+data_action_artisanat_int = [
+    {
+        title: "Réparer",
+        icon: "flat-hammer",
+        subtitle: "Pas de coût",
+        description: "Conditions. Vous avez un kit de réparation.",
+        reference: "LDB, pg. 242.",
+        bullets: [
+            "Vous passez dix minutes à tenter de réparer un objet endommagé, en le plaçant sur une surface stable et en utilisant le kit de réparation avec vos deux mains. Le MJ détermine le DD, sachant que le DD pour Réparer un objet donné est généralement similaire à celui pour le Fabriquer. Vous ne pouvez pas Réparer un objet détruit.",
+            "<b>Succès critique.</b> L’objet réparé récupère 10 points de vie, et 10 points de vie supplémentaires par rang de maîtrise que vous avez en Artisanat (pour un total de 20 PV si vous êtes qualifié, 30 PV si vous êtes un expert, 40 PV si vous êtes un maître ou 50 PV si vous êtes légendaire).",
+            "<b>Succès.</b> L’objet réparé récupère 5 points de vie, et 5 points de vie supplémentaires par rang de maîtrise que vous avez en Artisanat (pour un total de 10 PV si vous êtes qualifié, 15 PV si vous êtes un expert, 20 PV si vous êtes un maître ou 25 PV si vous êtes légendaire).",
+            "<b>Échec critique.</b> Vous infligez 2d6 dégâts à l’objet. Appliquez la Solidité de l’objet à ces dégâts."
+        ]
+    },
+	{
+        title: "Identifier l’alchimie",
+        icon: "dodge",
+        subtitle: "Pas de coût",
+        description: "Conditions. Vous avez des outils d’alchimiste.",
+        reference: "LDB, pg. 243.",
+        bullets: [
+            "Vous pouvez identifier la nature d’un objet alchimique en l’analysant pendant dix minutes avec vos outils d’alchimiste. Si votre tentative est interrompue, vous devez la reprendre depuis le début.",
+            "<b>Succès.</b> Vous identifiez l’objet et découvrez la méthode pour l’activer.",
+            "<b>Échec.</b> Vous ne parvenez pas à identifier l’objet mais vous pouvez recommencer.",
+            "<b>Échec critique.</b> Vous identifiez l’objet de travers et le prenez pour un autre objet au choix du MJ."
+        ]
+    },
+	 {
+        title: "Fabriquer",
+        icon: "shield-reflect",
+        subtitle: "Pas de coût",
+        reference: "LDB, pg. 242.",
+        bullets: [
+            "Vous pouvez fabriquer un objet avec des matières premières. Vous devez avoir le don de compétence Artisanat alchimique et un laboratoire d’alchimie pour créer des objets alchimiques, le don de compétence Artisanat magique pour créer des objets magiques et le don Fabrication de pièges pour créer des pièges. Pour fabriquer un objet, vous devez satisfaire aux conditions suivantes :",
+            "<ul>" +
+                "<li>L’objet est d’un niveau inférieur ou égal au vôtre. Un objet dont le niveau n’est pas précisé est de niveau 0. Vous devez être un maître en Artisanat si le niveau de l’objet est de 9 ou plus, et légendaire s’il est de niveau 16 ou plus.</li>" +
+                "<li>Vous possédez la formule de l’objet ; voir Obtenir des formules ci-après pour plus d’informations.</li>" +
+                "<li>Vous possédez un lot d’outils appropriés et un atelier est très souvent nécessaire. Par exemple, vous devez avoir accès à une forge pour forger un bouclier métallique.</li>" +
+                "<li>Vous devez fournir les matières premières d’une valeur au moins égale à la moitié du prix de l’objet. Vous dépensez toujours ce montant de matières premières au minimum quand vous parvenez à Fabriquer un objet. Si vous êtes dans une communauté, vous pouvez généralement acheter les matières premières nécessaires, sauf si elles sont précieuses et rares." +
+            "</ul>",
+            "Vous devez passer quatre jours à travailler et vous faites un test d’Artisanat à la fin de cette période. Le MJ détermine le DD pour Fabriquer l’objet en se basant sur son niveau, sa rareté et d’autres circonstances.",
+            "Si votre tentative pour fabriquer l’objet est réussie, vous dépensez les matières premières que vous avez fournies. Vous pouvez régler la partie restante du prix de l’objet en matières pour terminer immédiatement la fabrication de l’objet ou vous pouvez passer des jours d’intermède supplémentaires pour continuer le travail. Pour chaque jour supplémentaire que vous y consacrez, réduisez la valeur des matières que vous devez dépenser pour terminer la fabrication de l’objet. Après l’un de ces jours d’intermède, vous pouvez terminer la fabrication de l’objet en dépensant la partie restante de son prix en matières. Si les jours d’intermède que vous consacrez à la fabrication sont interrompus, vous pouvez vous remettre plus tard au travail et reprendre là où vous avez été interrompu.",
+            "<b>Succès critique.</b> Votre tentative est réussie. Chaque jour supplémentaire consacré à la fabrication réduit la valeur des matières nécessaires pour terminer l’objet d’un montant basé sur votre niveau + 1 et votre rang de maîtrise en Artisanat.",
+            "<b>Succès.</b> Votre tentative est réussie. Chaque jour supplémentaire consacré à la fabrication réduit la valeur des matières nécessaires pour terminer l’objet d’un montant basé sur votre niveau et votre rang de maîtrise.",
+            "<b>Échec.</b> Vous ne parvenez pas à terminer la fabrication de l’objet. Vous pouvez récupérer la pleine valeur des matières premières que vous avez fournies. Vous devez tout recommencer depuis le début si vous voulez refaire une tentative.",
+            "<b>Échec critique.</b> Vous ne parvenez pas à terminer la fabrication de l’objet. Vous perdez 10 % des matières premières que vous avez fournies mais vous récupérez le reste. Vous devez tout recommencer depuis le début si vous voulez refaire une tentative."
+        ]
+        
+    }
+
+]
---
+++ js/data_action_interaction.js
@@ -0,0 +1,98 @@
+data_action_interaction = [
+    {
+        title: "Chercher",
+        icon: "run",
+        subtitle: "Coût: 1 action",
+        reference: "LDB, pg. 471.",
+        bullets: [
+            "Vous scrutez une zone à la recherche de créatures ou d’objets. Si vous utilisez Chercher pour découvrir des objets, vous examinez une case de 3 m de côté maximum qui se trouve dans un emplacement adjacent.",
+            "<b>Succès critique.</b> Si vous tentiez de repérer des créatures, toute créature non détectée ou cachée contre laquelle vous avez obtenu un succès critique devient observée face à vous. Si vous cherchiez un objet, vous découvrez son emplacement.",
+            "<b>Succès.</b> Si vous tentiez de repérer des créatures, toute créature non détectée contre laquelle vous avez obtenu un succès devient cachée au lieu de non détectée et toute créature cachée devient observée. Si vous cherchiez un objet, vous découvrez son emplacement ou un indice vous aidant à trouver celui-ci, en fonction de ce que préfère le MJ."
+        ]
+    },
+	{
+        title: "Deviner les intentions",
+        icon: "walking-boot",
+        subtitle: "Coût: 1 action",
+        reference: "LDB, pg. 470.",
+        bullets: [
+            "Vous tentez de déterminer si le comportement d’une créature est anormal. Vous tentez de repérer des signes étranges, de la nervosité ou d’autres indicateurs qui peuvent laisser penser qu’elle tente de tromper quelqu’un.",
+            "<b>Succès critique.</b> Vous devinez les véritables intentions de la créature et déterminez le type de magie mental qui l’affecte éventuellement.",
+            "<b>Succès.</b> Vous êtes capable de dire si la créature se comporte normalement ou non. ",
+            "<b>Échec.</b> Vous détectez seulement ce qu’une créature trompeuse cherche à vous faire croire.",
+            "<b>Échec critique.</b> Votre impression concernant les intentions de la créature est erronée."
+        ]
+    },
+	 {
+        title: "Interagir",
+        icon: "sprint",
+        subtitle: "Coût: 1 action",
+        reference: "LDB, pg. 470.",
+        bullets: [
+            "Vous utilisez une main ou plus pour manipuler un objet ou un élément de votre environnement. Vous devrez probablement faire un test de compétences afin de déterminer le succès de votre interaction."
+        ]
+    },
+	    {
+        title: "Détourner le regard",
+        icon: "crawl",
+        subtitle: "Coût: 1 action",
+        reference: "LDB, pg. 471.",
+        bullets: [
+            "Vous détournez le regard d’un danger. Vous gagnez un bonus de circonstances de +2 à vos jets de sauvegarde contre les pouvoirs visuels qui nécessitent que vous regardiez une créature ou un objet, comme le regard pétrifiant d’une méduse. Votre regard reste détourné jusqu’au début de votre prochain tour."
+        ]
+    },
+	 {
+        title: "Relâcher",
+        icon: "crawl",
+        subtitle: "Coût: 1 action gratuite",
+        reference: "LDB, pg. 472.",
+        bullets: [
+            "Vous Relâchez quelque chose que vous tenez dans une main ou dans vos deux mains. À la différence des autres actions de manipulation, Relâcher ne déclenche pas de réactions qui peuvent être déclenchées par les actions possédant le trait manipulation. Si vous voulez vous tenir prêt à Relâcher quelque chose hors de votre tour, utilisez l’activité Préparer."
+        ]
+    },
+	    {
+        title: "Signaler",
+        icon: "crawl",
+        subtitle: "Coût: 1 action",
+        description: "Conditions. Une créature est non détectée par un ou plusieurs de vos alliés, mais pas par vous.",
+        reference: "LDB, pg. 472.",
+        bullets: [
+			"Vous désignez une créature que vous pouvez voir à un ou plusieurs de vos alliés, en la montrant du doigt et en indiquant verbalement à quelle distance elle se trouve. La créature se trouve alors cachée par rapport à vos alliés plutôt que non détectée. Cela ne fonctionne que si vos alliés peuvent vous voir et qu’ils se trouvent à un endroit d’où ils pourraient potentiellement détecter la cible. Si vos alliés ne peuvent pas vous entendre ou vous comprendre, ils doivent alors réussir un test de Perception contre le DD de Discrétion de la créature. Sur un échec, ils vous comprennent mal et croient que la cible se trouve à un autre endroit que celui que vous désignez."
+        ]
+    },
+	 {
+        title: "Préparer",
+        icon: "stone-pile",
+        subtitle: "Coût: 2 actions",
+        reference: "LDB, pg. 470.",
+        bullets: [
+            "Vous vous préparez à utiliser une action qui se déroulera hors de votre tour. Choisissez une action unique ou une action gratuite que vous pouvez utiliser et précisez un déclencheur. Votre tour prend alors fin. Si le déclencheur que vous avez indiqué se déroule avant le début de votre prochain tour, vous pouvez utiliser l’action choisie comme une réaction (dans la mesure où vous répondez à toutes les conditions d’utilisation). Vous ne pouvez pas Préparer une action gratuite qui a déjà un déclencheur.",
+            "Si vous êtes soumis à un malus d’attaques multiples et que votre action préparée est une action d’attaque, votre attaque préparée est soumise au malus d’attaques multiples que vous aviez quand vous l’avez Préparée. Il s’agit d’une des rares occasions où le malus d’attaques multiples est appliqué hors de votre tour."
+        ]
+    },
+    {
+        title: "Retarder",
+        icon: "horse-head",
+        subtitle: "Coût: 1 action gratuite",
+        description: "Déclencheur. Votre tour commence.",
+        reference: "LDB, pg. 472.",
+        bullets: [
+            "Vous attendez le bon moment pour agir. Vous pouvez reprendre une place dans l’ordre d’initiative par une action gratuite déclenchée par la fin du tour d’une créature. Cette nouvelle place devient votre place permanente dans l’ordre d’initiative. Vous ne pouvez pas utiliser de réaction avant de reprendre une place dans l’ordre d’initative.",
+            "Quand vous Retardez, tous les dégâts persistants et les autres effets négatifs qui se produisent normalement au début de votre tour sont appliqués immédiatement quand vous utilisez l’action Retarder.",
+            "Tous les effets bénéfiques qui devraient normalement prendre fin à n’importe quel moment de votre tour se terminent également."
+        ]
+    },
+	{
+        title: "Aider",
+        icon: "angel-wings",
+        subtitle: "Coût: 1 réaction",
+        description: "Conditions. L’allié est disposé à accepter votre aide et vous vous êtes préparer à l’aider.",
+        reference: "LDB, pg. 472.",
+        bullets: [
+            "Vous tentez d’apporter votre aide à un allié confronté à une tâche particulière. Pour utiliser cette réaction, vous devez préalablement vous être préparé à l’aider. Quand vous utilisez la réaction Aider, faites un test de compétence ou un jet d’attaque en lien avec l’action de votre allié.",
+            "<b>Succès critique.</b> Vous permettez à votre allié de bénéficier d’un bonus de circonstances de +2 au test déclencheur. Si vous possédez le rang de maîtrise de maître dans le test que vous faites, ce bonus est de +3 et, si votre rang est légendaire, il est de +4.",
+            "<b>Succès.</b> Vous permettez à votre allié de bénéficier d’un bonus de +1 au test déclencheur.",
+            "<b>Échec critique.</b> Votre allié subit un malus de -1 au test déclencheur"
+        ]
+    }
+]
