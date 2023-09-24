7bf07e28544adcc678db5c0143ad7397a57ce0e3
Padhiver <80126915+Padhiver@users.noreply.github.com>
2023-09-24 18:54:06 +0200

    Add files via upload

---
+++ js/data_action_arcane_int.js
@@ -0,0 +1,12 @@
+data_action_arcane_int = [
+    {
+        title: "Emprunter un sort",
+        icon: "bleeding-eye",
+        reference: "LDB, pg. 240.",
+        bullets: [
+            "Si vous êtes un incantateur arcanique qui prépare ses sorts écrits dans un grimoire, vous pouvez essayer de préparer un sort inscrit dans le grimoire de quelqu’un d’autre. Le MJ fixe le DD du test en fonction du niveau du sort et de sa rareté ; c’est généralement un peu plus simple que d’Apprendre le sort. ",
+            "<b>Succès.</b> Vous préparez le sort emprunté lors de votre préparation normale des sorts.",
+            "<b>Échec.</b> Vous ne parvenez pas à préparer le sort mais l’emplacement de sort reste disponible pour en préparer un autre. Vous ne pouvez pas tenter de préparer ce sort jusqu’à votre prochaine préparation des sorts."
+        ]
+    }
+]
---
+++ js/quickref.js
@@ -0,0 +1,86 @@
+function add_quickref_item(parent, data, type) {
+    var icon = data.icon || "perspective-dice-six-faces-one";
+    var subtitle = data.subtitle || "";
+    var title = data.title || "[no title]";
+
+    var item = document.createElement("div");
+    item.className += "item itemsize"
+    item.innerHTML =
+    '\
+    <div class="item-icon iconsize icon-' + icon + '"></div>\
+    <div class="item-text-container text">\
+        <div class="item-title">' + title + '</div>\
+        <div class="item-desc">' + subtitle + '</div>\
+    </div>\
+    ';
+
+    var style = window.getComputedStyle(parent.parentNode.parentNode);
+    var color = style.backgroundColor;
+
+    item.onclick = function () {
+        show_modal(data, color, type);
+    }
+
+    parent.appendChild(item);
+}
+
+function show_modal(data, color, type) {
+    var title = data.title || "[no title]";
+    var subtitle = data.description || data.subtitle || "";
+    var bullets = data.bullets || [];
+    var reference = data.reference || "";
+    type = type || "";
+    color = color || "black"
+
+    $("body").addClass("modal-open");
+    $("#modal").addClass("modal-visible");
+    $("#modal-backdrop").css("height", window.innerHeight + "px");
+    $("#modal-container").css("background-color", color).css("border-color", color);
+    $("#modal-title").text(title).append("<span class=\"float-right\">" + type + "</span>");
+    $("#modal-subtitle").html(subtitle);
+    $("#modal-reference").text(reference);
+
+    var bullets_html = bullets.map(function (item) { return "<p class=\"fonstsize\">" + item + "</p>"; }).join("\n<hr>\n");
+    $("#modal-bullets").html(bullets_html);
+}
+
+function hide_modal() {
+    $("body").removeClass("modal-open");
+    $("#modal").removeClass("modal-visible");
+}
+
+function fill_section(data, parentname, type) {
+    var parent = document.getElementById(parentname);
+    data.forEach(function (item) {
+        add_quickref_item(parent, item, type);
+    });
+}
+
+function init() {
+    fill_section(data_action_deplacement, "basic-movement", "Actions basiques");
+    fill_section(data_action_combat, "basic-actions", "Actions de combat");
+    fill_section(data_action_interaction, "basic-bonus-actions", "Actions d'interaction");
+    fill_section(data_action_magie, "basic-reactions", "Action de magie");
+    fill_section(data_action_objets, "basic-conditions", "Actions d'objets magiques");
+    fill_section(data_action_acrobatique_dex, "environment-obscurance", "Actions acrobatique (Dex)");
+    fill_section(data_action_arcane_int, "basic-arcane", "Actions d'arcanes (Int)");
+    fill_section(data_action_artisanat_int, "basic-artisanat", "Actions d'arcanes (Int)");
+    fill_section(data_action_athletisme_for, "basic-athletisme", "Actions d'arcanes (Int)");
+    fill_section(data_action_connaissances_int, "basic-connaissances", "Actions d'arcanes (Int)");
+    fill_section(data_action_diplomatie_cha, "basic-diplomatie", "Actions d'arcanes (Int)");
+    fill_section(data_action_discretion_dex, "basic-discretion", "Actions d'arcanes (Int)");
+    fill_section(data_action_duperie_cha, "basic-duperie", "Actions d'arcanes (Int)");
+    fill_section(data_action_intimidation_cha, "basic-intimidation", "Actions d'arcanes (Int)");
+    fill_section(data_action_medecine_sag, "basic-medecine", "Actions d'arcanes (Int)");
+    fill_section(data_action_nature_sag, "basic-nature", "Actions d'arcanes (Int)");
+    fill_section(data_action_representation_cha, "basic-representation", "Actions d'arcanes (Int)");
+    fill_section(data_action_societe_int, "basic-societe", "Actions d'arcanes (Int)");
+    fill_section(data_action_survie_sag, "basic-survie", "Actions d'arcanes (Int)");
+    fill_section(data_action_vol_dex, "basic-vol", "Actions d'arcanes (Int)");
+
+
+    var modal = document.getElementById("modal");
+    modal.onclick = hide_modal;
+}
+
+$(window).load(init);
